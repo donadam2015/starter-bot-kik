274698c257acb30f43bb57c3aab22d7843893ebd
Floren Henri <henri.floren@recast.ai>
2016-10-04 17:08:18 +0200

    [ADD] config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+let config = {}
+config.recast = {}
+config.recast.request_token = 'RECAST-TOKEN'
+config.recast.language = 'en' // to chose your language 'fr' or 'en'
+config.kik = {}
+config.kik.username = 'KIK-USERNAME'
+config.kik.apiKey = 'KIK-APIKEY'
+config.kik.baseUrl = 'NGROK-URL'
+
+module.exports = config
